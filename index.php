3986019d3703ed7a5a078f3b68855b62fb2d1df5
Mahdi Yari <m.yary15@gmail.com>
2018-02-02 17:31:29 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,63 @@
 <?php
 $active =0;
-require_once('header.php');
+require_once('he_ad.php');
 ?>
-    <!-- Full Width Image Header -->
+<!-- Full Width Image Header -->
 
 
-    <!-- Page Content -->
-    <div class="content">
+<!-- Page Content -->
+<div style="margin-top:20px;" class="row">
 
 
-        <!-- First Featurette -->
-	
-	<div class="col-md-6">
-        <div class="card">
-        <div class="content">
-		
-            
-			
-            <h2 class="featurette-heading">Curation Trail by
-                <i style="color:#0179ff;" class="text-muted">Steem<i style="color:#05ab05;">Auto</i></i>
-            </h2>
-            <p class="lead">You will follow other Trailer's upvotes or you will upvote other steemian Posts by custom weight. such as streemian.</p>
-   
-		
+	<!-- First Featurette -->
 
-        </div>
-        </div>
-        </div>	<div class="col-md-6">
-		<div class="card">
-        <div class="content">
-            <iframe width="100%" height="315" src="https://www.youtube.com/embed/fi5qayhXCdA" frameborder="0" allowfullscreen></iframe>This video created by <a target="_blank" href="https://steemit.com/@nomadics">@nomadics</a>
-	
-</div>
-	</div>
-	</div>
+	<div class="col-md-12">
+		<div class="col-md-6">
+			<div class="content">
+				<div class="card">
+					<div class="content">
+					
+						<h4>Welcome to the world of automation!</h4>
+						<h2 class="featurette-heading">
+							<i style="color:#0179ff;" class="text-muted">Steem<i style="color:#05ab05;">Auto</i></i>
+						</h2>
+						<p class="lead">SteemAuto comes with amaizing features, Schedule post, Build a Fanbase, or leave a curation trail for users all around the world to follow. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="content">
+				<div class="card">
+					<div class="content">
+						
+						<iframe width="100%" height="315" src="https://www.youtube.com/embed/fi5qayhXCdA" frameborder="0" allowfullscreen></iframe>
+						<sub>This video created by <a target="_blank" href="https://steemit.com/@nomadics">@nomadics</a></sub>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12">
+			<div class="content">
+				<div class="card">
+					<div class="content">
+
+						<p class="lead"><strong>Schedule Posts:</strong><br> Your time is money...schedule your post in advance, with the help of SteemAuto we'll publish your post whenever you want. <br>Write and plan in advance. .. Leave the rest to Steemauto.</p>
+						<p class="lead"><strong>Fanbase and Curation trail:</strong><br>You can follow famous authors and thier trails, or support them by fanbase, upvote them automatically when they have published a new post. (This will maximise your curation reward)<br>The upvote weight can be adjusted manually. </p>
+						<p class="lead"><strong>Claiming Rewards:</strong><br> Don't have time to check your steem blog? No worries! we can claim (redeem) the post rewards for you. (Your post reward will go into your steem wallet).</p>
+						<sub>introduction text contributed by @kingjan</sub>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>	
 
 
-</div>
+
+
 
 <?php
-require('footer.php');
+require('fo_oter.php');
 ?>
