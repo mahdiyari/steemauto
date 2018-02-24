fe61385ddc3a72addd6ca9d71375cad111a78dea
Carn507 <joellikesgiraffes@gmail.com>
2018-02-24 16:15:06 -0600

    Fixed grammar errors in index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once('he_ad.php');
 						<h2 class="featurette-heading">
 							<i style="color:#0179ff;" class="text-muted">Steem<i style="color:#05ab05;">Auto</i></i>
 						</h2>
-						<p class="lead">SteemAuto comes with amaizing features, Schedule post, Build a Fanbase, or leave a curation trail for users all around the world to follow. </p>
+						<p class="lead">SteemAuto comes with many amazing features. You can use it to schedule posts, build fanbases, create curation trails, and automatically claim your rewards. </p>
 					</div>
 				</div>
 			</div>
@@ -32,7 +32,7 @@ require_once('he_ad.php');
 					<div class="content">
 						
 						<iframe width="100%" height="315" src="https://www.youtube.com/embed/fi5qayhXCdA" frameborder="0" allowfullscreen></iframe>
-						<sub>This video created by <a target="_blank" href="https://steemit.com/@nomadics">@nomadics</a></sub>
+						<sub>This video was created by <a target="_blank" href="https://steemit.com/@nomadics">@nomadics</a></sub>
 						
 					</div>
 				</div>
@@ -43,9 +43,9 @@ require_once('he_ad.php');
 				<div class="card">
 					<div class="content">
 
-						<p class="lead"><strong>Schedule Posts:</strong><br> Your time is money...schedule your post in advance, with the help of SteemAuto we'll publish your post whenever you want. <br>Write and plan in advance. .. Leave the rest to Steemauto.</p>
-						<p class="lead"><strong>Fanbase and Curation trail:</strong><br>You can follow famous authors and thier trails, or support them by fanbase, upvote them automatically when they have published a new post. (This will maximise your curation reward)<br>The upvote weight can be adjusted manually. </p>
-						<p class="lead"><strong>Claiming Rewards:</strong><br> Don't have time to check your steem blog? No worries! we can claim (redeem) the post rewards for you. (Your post reward will go into your steem wallet).</p>
+						<p class="lead"><strong>Schedule Posts:</strong><br> Your time is money, so schedule your posts in advance, and with the help of SteemAuto you can publish your post whenever you want. <br>Write and plan in advance...leave the rest to SteemAuto.</p>
+						<p class="lead"><strong>Fanbase and Curation Trails:</strong><br>Follow curation trails and automatically upvote posts that your trail upvotes, or add your favorite authors to your fanbase and automatically upvote every one of their posts. (The upvote weight can be adjusted manually) Using these two features can help maximise your curation rewards.<br></p>
+						<p class="lead"><strong>Claiming Rewards:</strong><br> Don't have time to check your Steem blog? No worries! We can claim the post rewards for you and automatically send them to your Steem wallet.</p>
 						<sub>introduction text contributed by @kingjan</sub>
 					</div>
 				</div>
