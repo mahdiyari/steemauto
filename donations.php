0493fa3cb0067a62f68d581ed1911a7e40c32b0a
Mahdi Yari <m.yary15@gmail.com>
2018-05-31 16:18:42 +0430

    Update donations

--- donations.php
+++ donations.php
@@ -33,15 +33,25 @@ label{
 		<div style="" class="col-md-8">
 			<div class="card">
 				<div class="content">
+					<h4 style="border-bottom:1px solid #000;">Support Steemauto</h4>
+					<p>Hello, Steemauto users. As you know, Steemauto is a free service for all steem users and we don't have any earnings from Steemauto.
+						 But, we need to pay for our servers. I know, that is not your business. Just if you can, help us to keep this service free forever.
+					</p>
+					<h4 style="border-bottom:1px solid #000;">Witness vote</h4>
+					<p>You can support this project by voting @mahdiyari as a steem witness.
+					 A witness receives small rewards from steem blockchain depending on witness votes.
+					 <br />
+					 <a href="https://steemconnect.com/sign/account-witness-vote?witness=mahdiyari&approve=1">Click here to vote by steemconnect</a>
+					</p>
 					<h4 style="border-bottom:1px solid #000;">Donations</h4>
-					<p>Hello, Steemauto users. As you know, Steemauto is a free service for all steem users and we don't have any earnings from Steemauto. But, we need to pay for our servers. I know, that is not your business. Just if you can, help us to keep this service free forever.</p>
 					<h5>Our bills:</h5>
 					<p>1- 256 GB RAM (hetzner): $200/month</p>
-					<p>2- 15 GB RAM (scaleway): $40~$50/month</p>
+					<p><del>2- 15 GB RAM (scaleway): $40~$50/month</del></p>
+					<p>2- Soon a new server: +$100</p>
 					<p>3- cloudflare: $10/month</p>
 					<p>4- Mastercard/Visa: ~$20/month</p>
 					<hr>
-					<p>Total bill: $270~$300/month</p>
+					<p>Total bill: $250~$300/month</p>
 					<h5>This month goal: $300</h5>
 					<p>Donations for this month: $<?php echo $donations; ?> (Reload page for update)</p>
 					<br><br>
@@ -50,14 +60,14 @@ label{
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=1%20SBD">1 SBD</a>
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=5%20SBD">5 SBD</a>
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20SBD">10 SBD</a>
-					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20SBD">20 SBD</a>
-					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20SBD">50 SBD</a>
+					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=20%20SBD">20 SBD</a>
+					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=50%20SBD">50 SBD</a>
 					<br><br>
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=1%20STEEM">1 STEEM</a>
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=5%20STEEM">5 STEEM</a>
 					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20STEEM">10 STEEM</a>
-					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20STEEM">20 STEEM</a>
-					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=10%20STEEM">50 STEEM</a>
+					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=20%20STEEM">20 STEEM</a>
+					<a class="btn btn-primary" href="https://steemconnect.com/sign/transfer?to=steemauto&amount=50%20STEEM">50 STEEM</a>
 				</div>
 			</div>
 		</div>
