855b3808ba69d68a9a8e25e3a7b944d129c1b42a
Mahdi Yari <m.yary15@gmail.com>
2018-07-12 08:59:05 +0430

    Improved upvote functions!

--- nodejs/upvote.js
+++ nodejs/upvote.js
@@ -2,66 +2,85 @@ const config = require('./steemauto/config')
 // const steem = require("steem");
 const dsteem = require('dsteem')
 const http = require('http')
-var url = require('url')
-const hostname = '0.0.0.0'
-const port = 7412
+const url = require('url')
+const hostname = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 7412
 // steem.api.setOptions({ url: config.rpc });
-var client = new dsteem.Client(config.rpchttp)
+const client = new dsteem.Client(config.rpchttp)
 
-function upvote (wif, voter, author, permlink, weight, callback) {
-  var key = dsteem.PrivateKey.from(wif)
-  client.broadcast.vote({
-  	voter: voter,
-  	author: author,
-  	permlink: permlink,
-  	weight: weight
-  }, key).then(function (result) {
-	   callback(1)
-  }, function (error) {
-	   callback(0)
-  })
+// We will handle upvotes by this function
+const upvote = async (wif, voter, author, permlink, weight) => {
+  let key = dsteem.PrivateKey.from(wif)
+  let vote = await client.broadcast.vote({
+    voter,
+    author,
+    permlink,
+    weight
+  }, key)
+  if (vote) return 1
+  else throw new Error('up failed')
 }
+
+// This server will receive wif, voter, author, permlink and weight
+// Then will return result of upvote
 const server = http.createServer((req, res) => {
-  var params = url.parse(req.url, true).query
+  let params = url.parse(req.url, true).query
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
 
-  var wif = params.wif
-  var voter = params.voter
-  var author = params.author
-  var permlink = params.permlink
-  var weight = parseInt(params.weight)
-  if (weight <= 0) { weight = 1 };
+  let wif = params.wif
+  let voter = params.voter
+  let author = params.author
+  let permlink = params.permlink
+  let weight = parseInt(params.weight)
+  if (weight <= 0) weight = 1
   if (wif && voter && author && permlink && weight) {
+    // We will check voters and post date
+    // We will skip already upvoted posts and posts older than 6.5 days
     client.database.call('get_content', [author, permlink])
       .then(function (result) {
-        var datee = new Date()
-        var secondss = datee.getTime() / 1000
-        var datee1 = new Date(result.created + 'Z')
-        var secondss1 = datee1.getTime() / 1000
+        let datee = new Date()
+        let secondss = datee.getTime() / 1000
+        let datee1 = new Date(result.created + 'Z')
+        let secondss1 = datee1.getTime() / 1000
         if ((secondss - secondss1) < 561600) {
-          var voted = 0
-          for (j in result['active_votes']) {
-            if (result['active_votes'][j].voter == voter) {
+          // 561600 seconds = 6.5 days
+          let voted = 0
+          for (let j in result['active_votes']) {
+            if (result['active_votes'][j].voter === voter) {
               voted = 1
               break
             }
           }
-          if (voted == 0) {
-            upvote(wif, voter, author, permlink, weight, function (result) {
-              if (result == 1) {
-                res.end(JSON.stringify({ result: 0, reason: 'up done' }))
-              } else {
-                res.end(JSON.stringify({ result: 0, reason: 'up fail' }))
-              }
-            })
+          if (voted === 0) {
+            upvote(wif, voter, author, permlink, weight)
+              .then(result => {
+                res.status(200).json({
+                  result: 1,
+                  reason: 'up done'
+                })
+              })
+              .catch(err => {
+                res.status(200).json({
+                  result: 0,
+                  reason: 'up fail',
+                  err
+                })
+              })
           } else {
-            res.end(JSON.stringify({ result: 0, reason: 'already voted' }))
+            res.status(200).json({
+              result: 0,
+              reason: 'already voted'
+            })
           }
         }
-  	}, function (error) {
-  	 	res.end(JSON.stringify({ result: 0, reason: 'rpc node fail' }))
-  	})
+      }).catch(err => {
+        res.status(200).json({
+          result: 0,
+          reason: 'rpc node fail',
+          err
+        })
+      })
   }
 })
 server.listen(port, hostname, () => {
