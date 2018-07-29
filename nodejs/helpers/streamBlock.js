8a09d6dbacc77433b65d53d7cca67f4a269dd315
Mahdi Yari <m.yary15@gmail.com>
2018-07-29 16:56:48 +0430

    Create streamBlock.js

---
+++ nodejs/helpers/streamBlock.js
@@ -0,0 +1,39 @@
+import { call } from './nodeCall.js'
+import config from '../config'
+
+export const streamBlockNumber = async (cb) => {
+  let lastBlock = 0
+  setInterval(async () => {
+    const result = await call(
+      config.steemd,
+      'condenser_api.get_dynamic_global_properties',
+      []
+    )
+    if (result && result.head_block_number && !isNaN(result.head_block_number)) {
+      if (result.head_block_number > lastBlock) {
+        lastBlock = result.head_block_number
+        cb(lastBlock)
+      }
+    }
+  }, 500)
+}
+
+export const streamBlockOperations = async (cb) => {
+  streamBlockNumber(async blockNumber => {
+    const result = await call(
+      config.steemd,
+      'condenser_api.get_block',
+      [blockNumber]
+    )
+    if (result) {
+      const operations = result.transactions.map(transaction => {
+        return transaction.operations
+      })
+      if (operations.length > 0) {
+        for (let operation of operations) {
+          cb(operation)
+        }
+      }
+    }
+  })
+}
