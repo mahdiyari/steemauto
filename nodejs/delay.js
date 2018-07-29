3ad2d013ae418ab53d0a6f11832e2921603913ab
Mahdi Yari <m.yary15@gmail.com>
2018-07-29 22:52:50 +0430

    used custom methods, used MySQL pool

--- nodejs/delay.js
+++ nodejs/delay.js
@@ -1,93 +1,145 @@
 const config = require('./config')
-// var steem = require('steem');
-var mysql = require('mysql')
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
-var con = mysql.createConnection({host: config.db.host, user: config.db.user, password: config.db.pw, database: config.db.name, charset: 'utf8mb4'})
-var wifkey = config.wifkey
+const con = require('./mysql')
+const fetch = require('node-fetch')
+const call = require('./helpers/nodeCall')
 
-// steem.api.setOptions({ url: config.rpc });
+const wifkey = config.wifkey
 
-// Check voting power limit
-function checkpowerlimit (voter, author, permlink, weight, type) {
-  con.query('SELECT `current_power`,`limit_power`,`sp` FROM `users` WHERE `user`="' + voter + '"', function (error, results, fields) {
-    for (i in results) {
-      var powernow = results[i].current_power
-      var powerlimit = results[i].limit_power
-      var sp = results[i].sp
-      if (powernow > powerlimit) {
-        if (((powernow / 100) * (weight / 10000) * sp) > 1.5) { // Don't broadcast upvote if sp*weight*power < 1.5
-          upvote(voter, author, permlink, weight)
-        } else {
-          // console.log('Low SP');
-        }
-      } else {
-        // console.log('power is under limit user '+voter);
-      }
-    }
-  })
-
-  return 1
-}
-// Upvote function - included 0 seconds delay!
-var delay = 0
-function upvote (voter, author, permlink, weight, type) {
-  var xmlhttp = new XMLHttpRequest()
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      if (JSON.parse(this.responseText).result == 1) {
-        // console.log('up done');
-      } else {
-        // console.log(JSON.parse(this.responseText).reason);
-      }
-    }
-  }
-  xmlhttp.open('GET', config.nodejssrv + ':7412/?wif=' + wifkey + '&voter=' + voter + '&author=' + author + '&permlink=' + permlink + '&weight=' + weight, true)
-  xmlhttp.send()
-
-  return 1
-}
-
-// Upvoting After a Delay
-setInterval(function () {
+// we will check database every 10 seconds to broadcast delayed upvotes
+setInterval(async () => {
   try {
-    var datee = new Date()
-    var secondss = datee.getTime() / 1000
-    var now = Math.floor(secondss)
-    con.query('SELECT EXISTS(SELECT * FROM `upvotelater` WHERE `time`<"' + now + '")', function (error, results, fields) {
-      for (k in results) {
-        for (j in results[k]) {
-          var x = results[k][j]
-          if (x == 1) {
-            con.query('SELECT * FROM `upvotelater` WHERE `time`<"' + now + '"', function (error, results, fields) {
-              for (i in results) {
-                var voter = results[i].voter
-                var author = results[i].author
-                var permlink = results[i].permlink
-                var weight = results[i].weight
-                var id = results[i].id
-                var type = results[i].trail_fan
-
-                checkpowerlimit(voter, author, permlink, weight, type)
-
-                // console.log('delaied to up.');
-                removing(id)
-              }
-            })
-          }
+    const nowdate = new Date()
+    const nowsec = nowdate.getTime() / 1000
+    const now = Math.floor(nowsec)
+    const results = await con.query(
+      'SELECT EXISTS(SELECT `id` FROM `upvotelater` WHERE `time`<?)',
+      [now]
+    )
+    for (let i in results[0]) {
+      const exists = results[0][i]
+      // is there any data in the database?
+      if (exists === 1) {
+        const results = await con.query(
+          'SELECT `voter`,`author`,`permlink`,`weight`,`id` FROM `upvotelater` WHERE `time`<?',
+          [now]
+        )
+        for (let i in results) {
+          const voter = results[i].voter
+          const author = results[i].author
+          const permlink = results[i].permlink
+          const weight = results[i].weight
+          const id = results[i].id
+          // first check limitations
+          // then broadcast upvote
+          // and remove that upvote from database
+          checkpowerlimit(voter, author, permlink, weight)
+          removing(id)
         }
       }
-    })
+    }
   } catch (e) {
-    console.log('error in Delay Upvoting.')
+    throw new Error(e)
   }
 }, 10000)
 
-function removing (id) {
-  con.query('DELETE FROM `upvotelater` WHERE `upvotelater`.`id` = "' + id + '"', function (error, results, fields) {})
-  return 1
+let tvfs
+let tvs
+// Check voting power limit
+// Since this function is same in the 3 files, soon we will export it to another file
+const updateGlobals = async () => {
+  try {
+    // get dynamic global propertise for just appbase! (v0.19.10)
+    // will need change in other version nodes
+    const result = await call(
+      config.steemd,
+      'condenser_api.get_dynamic_global_properties',
+      []
+    )
+    // on any error, result will be null
+    if (!result) return 1
+    tvfs = parseInt(result.total_vesting_fund_steem.replace('STEEM', ''))
+    tvs = parseInt(result.total_vesting_shares.replace('VESTS', ''))
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+updateGlobals()
+setInterval(() => {
+  updateGlobals()
+}, 900000)
+
+// Check limitations
+// Since this function is same in the 3 files, soon we will export it to another file
+const checkpowerlimit = async (voter, author, permlink, weight, type) => {
+  try {
+    const results = await con.query(
+      'SELECT `limit_power` FROM `users` WHERE `user`=?',
+      [voter]
+    )
+    const powerlimit = results[0].limit_power
+    // Get accounts information from appbase (v0.19.10)
+    // will need change in other versions
+    const result = await call(
+      config.steemd,
+      'condenser_api.get_accounts',
+      [
+        [voter]
+      ]
+    )
+    // on any error, result will be null
+    if (!result) return 1
+    if (tvfs && tvs) {
+      // calculating voting power to check limitation
+      const u = result[0]
+      const now = new Date()
+      const n = now.getTime() / 1000
+      const last = new Date(u.last_vote_time + 'z')
+      const l = last.getTime() / 1000
+      const power = u.voting_power / 100 + (parseFloat(n - l) / 4320)
+      let powernow = power.toFixed(2)
+      if (powernow > 100) powernow = 100
+      // calculating total SP to check limitation
+      const delegated = parseInt(u.delegated_vesting_shares.replace('VESTS', '')) // VESTS
+      const received = parseInt(u.received_vesting_shares.replace('VESTS', '')) // VESTS
+      const vesting = parseInt(u.vesting_shares.replace('VESTS', '')) // VESTS
+      const totalvest = vesting + received - delegated
+      let sp = totalvest * (tvfs / tvs)
+      sp = sp.toFixed(2)
+      if (powernow > powerlimit) {
+        if (((powernow / 100) * (weight / 10000) * sp) > 1.5) {
+          // Don't broadcast upvote if sp*weight*power < 1.5
+          upvote(voter, author, permlink, weight)
+        }
+      }
+    }
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+// Upvote function
+// Since this function is same in the 3 or more files, soon we will export it to another file
+const upvote = async (voter, author, permlink, weight) => {
+  try {
+    // Upvote server url for handling upvotes
+    const url = config.nodejssrv + ':7412/' +
+      '?wif=' + wifkey +
+      '&voter=' + voter +
+      '&author=' + author +
+      '&permlink=' + permlink +
+      '&weight=' + weight
+    await fetch(url)
+  } catch (e) {
+    throw new Error(e)
+  }
 }
 
-setInterval(function () {
-  con.query('SELECT 1', function (error, results, fields) {})
-}, 5000)
-console.log('Delay Started.')
+const removing = async (id) => {
+  con.query(
+    'DELETE FROM `upvotelater` WHERE `upvotelater`.`id` = ?',
+    [id]
+  ).catch(e => {
+    throw new Error(e)
+  })
+}
+
+console.log('Delay upvoting Started.')
